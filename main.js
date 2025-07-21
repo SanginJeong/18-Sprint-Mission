660a433f9d230c40bb7d6030991aca7ffe926d43
sanginjeong <sanginjeong07@gmail.com>
2025-07-21 16:44:18 +0900

    feat: login Button State Update

--- main.js
+++ main.js
@@ -3,15 +3,18 @@ const $loginEmail = document.querySelector("#login-email");
 const $loginPassword = document.querySelector("#login-password");
 const $loginBtn = document.querySelector("#login-btn");
 
+let isValidLoginEmailValue = false;
+let isValidLoginPasswordValue = false;
+
 const clearErrorTag = (element) => {
   const errorTag = element.nextElementSibling;
-  if(errorTag && errorTag.classList.contains("error-message")){
+  if (errorTag && errorTag.classList.contains("error-message")) {
     element.classList.remove("error-status");
     errorTag.remove();
   }
-}
+};
 
-const createErrorTag = (element,message) => {
+const createErrorTag = (element, message) => {
   const tag = document.createElement("p");
   tag.classList.add(`error-message`);
   tag.textContent = message;
@@ -22,37 +25,51 @@ const createErrorTag = (element,message) => {
 const isValidEmail = (email) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
-}
-
-// $loginEmail.addEventListener("input", (e)=>console.log(e.target.value));
+};
 
 $loginEmail.addEventListener("focusout", (e) => {
   e.preventDefault();
   clearErrorTag(e.target);
-  const { value } = e.target
+  const { value } = e.target;
   if (!value) {
-    const errorTag = createErrorTag(e.target,"이메일을 입력해주세요");
+    const errorTag = createErrorTag(e.target, "이메일을 입력해주세요");
+    $loginBtn.disabled = true;
     return errorTag;
   }
 
-  if(!isValidEmail(value)) {
-    const errorTag = createErrorTag(e.target,"잘못된 이메일 형식입니다");
+  if (!isValidEmail(value)) {
+    const errorTag = createErrorTag(e.target, "잘못된 이메일 형식입니다");
+    $loginBtn.disabled = true;
     return errorTag;
+  }
+  isValidLoginEmailValue = true;
+
+  if (isValidLoginPasswordValue) {
+    $loginBtn.disabled = false;
   }
 });
 
 $loginPassword.addEventListener("focusout", (e) => {
   e.preventDefault();
   clearErrorTag(e.target);
-  const {value} = e.target;
-  if(!value) {
+  const { value } = e.target;
+  if (!value) {
     const errorTag = createErrorTag(e.target, "비밀번호를 입력해주세요");
+    $loginBtn.disabled = true;
     return errorTag;
   }
 
-  if(value.length < 8) {
-    const errorTag = createErrorTag(e.target, "비밀번호를 8자 이상 입력해주세요.");
+  if (value.length < 8) {
+    const errorTag = createErrorTag(
+      e.target,
+      "비밀번호를 8자 이상 입력해주세요."
+    );
+    $loginBtn.disabled = true;
     return errorTag;
+  }
+  isValidLoginPasswordValue = true;
+  if (isValidLoginEmailValue) {
+    $loginBtn.disabled = false;
   }
 });
 
@@ -60,8 +77,3 @@ $loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
   window.location.href = "/items";
 });
-
-
-// input 에 빈 값이 있거나 에러 메세지가 있으면  ‘로그인’ 버튼은 비활성화 됩니다.
-// Input 에 유효한 값을 입력하면  ‘로그인' 버튼이 활성화 됩니다.
-// 활성화된 ‘로그인’ 버튼을 누르면  “/items” 로 이동합니다

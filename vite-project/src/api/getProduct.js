33109c349d4f4ff4e4c0774837095701ba1e549b
sanginjeong <sanginjeong07@gmail.com>
2025-08-06 22:31:48 +0900

    refactor: getProduct params 구체화

--- vite-project/src/api/getProduct.js
+++ vite-project/src/api/getProduct.js
@@ -1,8 +1,12 @@
 import { instance } from "./axiosInstance";
 
-export const getProduct = async (params) => {
+export const getProduct = async ({ page, orderBy, pageSize }) => {
   try {
-    const { data } = await instance.get("/products", { params });
+    const { data } = await instance.get("/products", {
+      page,
+      orderBy,
+      pageSize,
+    });
     return data;
   } catch (error) {
     console.error(error);

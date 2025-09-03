cafb4fa66dc10d0cabeabcf9d65fe66f2f3918f9
sanginjeong <sanginjeong07@gmail.com>
2025-09-03 14:31:45 +0900

    refactor: 상품 문의하기 div -> form 으로 변경

--- vite-project/src/pages/ProductDetailPage/components/ProductQuestion/index.jsx
+++ vite-project/src/pages/ProductDetailPage/components/ProductQuestion/index.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 const ProductQuestion = () => {
   const [textAreaValue, setTextAreaValue] = useState("");
 
+  const handleSubmitQuestion = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <div className="productDetail-question-category-textarea">
@@ -14,14 +18,14 @@ const ProductQuestion = () => {
           placeholder="개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다."
         />
       </div>
-      <div className="add-question-btn-area">
+      <form onSubmit={handleSubmitQuestion} className="add-question-btn-area">
         <button
           className="button btn-small-40 add-question-btn"
           disabled={!textAreaValue}
         >
           등록
         </button>
-      </div>
+      </form>
     </>
   );
 };

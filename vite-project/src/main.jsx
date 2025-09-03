2163fe831b65a3821339cdd4a3c80f5ca4ada4d9
sanginjeong <sanginjeong07@gmail.com>
2025-09-03 17:24:39 +0900

    feat: useNavigate 페이지 이동시 스크롤 최상단 이동 구현

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,12 +2,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
+import ScrollToTop from "./common/ScrollToTop/index.jsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </QueryClientProvider>

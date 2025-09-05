5ccaf3b401707e398847953ea99f8ca9c7ca73f1
sanginjeong <sanginjeong07@gmail.com>
2025-09-05 13:55:54 +0900

    feat: main Navbar 디자인

--- vite-project/src/common/MainNavbar/index.jsx
+++ vite-project/src/common/MainNavbar/index.jsx
@@ -1,5 +1,23 @@
+import "./MainNavbar.style.css";
+import { Link } from "react-router";
+
 const MainNavbar = () => {
-  return <div>index</div>;
+  return (
+    <header className="header">
+      <nav className="nav">
+        <div className="nav-main-left">
+          <img src="/images/panda-face.svg" alt="메인 판다 얼굴" />
+          <Link className="nav-link-home" to="/">
+            판다마켓
+          </Link>
+        </div>
+
+        <div className="nav-main-right">
+          <button className="button btn-small-48 nav-login-btn">로그인</button>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default MainNavbar;

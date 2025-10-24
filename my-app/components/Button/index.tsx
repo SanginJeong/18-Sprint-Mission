7530b5bbc93d9483bfe9dd55ea71a85fdb443c25
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-10-25 03:52:34 +0900

    feat: Button 컴포넌트 구현

--- my-app/components/Button/index.tsx
+++ my-app/components/Button/index.tsx
@@ -5,10 +5,6 @@ import IcPlus from "@/public/images/ic_plus.svg";
 import IcDelete from "@/public/images/ic_x.svg";
 import IcUpdate from "@/public/images/ic_check.svg";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variants: "append" | "delete" | "update";
-}
-
 const variantsToLabel = {
   append: "추가하기",
   delete: "삭제하기",
@@ -20,6 +16,10 @@ const variantsToIcon = {
   delete: <Image src={IcDelete} alt="삭제하기" aria-label="항목 삭제하기" />,
   update: <Image src={IcUpdate} alt="수정완료" aria-label="항목 수정완료" />,
 };
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variants: keyof typeof variantsToLabel;
+}
 
 const Button = ({ variants, ...props }: ButtonProps) => {
   return (

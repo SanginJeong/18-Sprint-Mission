941126e7203110fb44f064e4c00ab09c6f67f415
SanginJeong <144919938+SanginJeong@users.noreply.github.com>
2025-10-25 09:32:29 +0900

    feat: todo 구현

---
+++ my-app/components/Todo/index.tsx
@@ -0,0 +1,36 @@
+import { HTMLAttributes, MouseEvent } from "react";
+import styles from "./Todo.module.css";
+import CheckBox from "../CheckBox";
+
+export interface TodoItem {
+  id: number;
+  isCompleted: boolean;
+  name: string;
+}
+
+interface TodoProps extends TodoItem {
+  onTodoClick: () => void;
+  onCheckboxClick: (e: MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Todo = ({
+  id,
+  isCompleted,
+  name,
+  onTodoClick,
+  onCheckboxClick,
+  ...props
+}: TodoProps) => {
+  return (
+    <span
+      className={`${styles.todo} ${isCompleted && styles.done} font-regular-16`}
+      onClick={onTodoClick}
+      aria-label="todo"
+    >
+      <CheckBox isCompleted={isCompleted} onClick={onCheckboxClick} />
+      <span aria-label={name}>{name}</span>
+    </span>
+  );
+};
+
+export default Todo;
--- my-app/pages/test/index.tsx
+++ my-app/pages/test/index.tsx
@@ -1,6 +1,7 @@
 import Badge from "@/components/Badge";
 import Button from "@/components/Button";
 import CheckBox from "@/components/CheckBox";
+import Todo from "@/components/Todo";
 
 const Components = () => {
   return (
@@ -17,6 +18,28 @@ const Components = () => {
       <div style={{ padding: "40px 0px" }}>
         <CheckBox isCompleted={false} />
         <CheckBox isCompleted={true} />
+      </div>
+      <div style={{ padding: "40px 0px" }}>
+        <Todo
+          id={0}
+          name="비타민 챙겨먹기"
+          isCompleted={false}
+          onTodoClick={() => console.log("Todo")}
+          onCheckboxClick={(e) => {
+            e.stopPropagation();
+            console.log("체크박스");
+          }}
+        />
+        <Todo
+          id={1}
+          name="비타민 챙겨먹기"
+          isCompleted={true}
+          onTodoClick={() => console.log("Todo")}
+          onCheckboxClick={(e) => {
+            e.stopPropagation();
+            console.log("체크박스");
+          }}
+        />
       </div>
     </div>
   );
